10d92656d11a358b8e1a92be9505db6ff261d77b
James <james.michalec58@gmail.com>
2021-01-31 12:00:38 -0800

    commit tree bab8ef838dacfd5c0c98bc3aee43f82db0dd3a57

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require "pry"
+class Dog
+    @@all = []
+    attr_accessor :name, :dog_name
+
+    def initialize(name)
+        @name=name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+    
+    
+    
+    
+    def self.all 
+        @@all
+    end
+
+    def self.clear_all
+        @@all = []
+    end    
+
+    def self.print_all
+        @@all.each do |dog_object|
+            puts dog_object.name
+        end
+    end
+
+
+
+end
